47b2727f6d3aa2ef36d1e48513b1cf74a279dacb
Majkic <vrnebojsamajkic@gmail.com>
2020-05-21 22:57:14 +0200

    Novi pocetak

---
+++ src/app/Main.java
@@ -0,0 +1,23 @@
+package app;
+
+import gui.MainFrame;
+import resource.data.Row;
+
+import javax.swing.*;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        AppCore appCore = new AppCore();
+        MainFrame mainFrame = MainFrame.getInstance();
+        mainFrame.setAppCore(appCore);
+
+
+        //mainFrame.getAppCore().readDataFromTable("dbo.DEPARTMENTS");
+        //mainFrame.getAppCore().loadResource();
+
+
+    }
+
+}
---
+++ src/utils/Constants.java
@@ -0,0 +1,10 @@
+package utils;
+
+public class Constants {
+
+    public static final String MSSQL_IP = "147.91.175.155";
+    public static final String MSSQL_DATABASE = "tim_8_bp2020";
+    public static final String MSSQL_USERNAME = "tim_8_bp2020";
+    public static final String MSSQL_PASSWORD = "L6WBLQ63";
+
+}
